af563fba932ee04ab246c0546da66b1a28ed4b45
Andreas Ulbrich <Andreas Ulbrich@DELL-ANDREAS-19>
2020-08-18 16:53:26 +0200

    added start scripts

---
+++ launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# PROJECT   : Elbfisch.org
+# MODULE    : launch.sh
+# VERSION   : 1.0
+# DATE      : 10/5/2016
+# PURPOSE   : 
+# AUTHOR    : Bernd Schuster, MSK Gesellschaft fuer Automatisierung mbH, Schenefeld
+# REMARKS   : -
+# CHANGES   : CH#n <Kuerzel> <datum> <Beschreibung>
+# LOG       : $Log$
+
+java -cp "./lib/*:../cfg/" org.elbfisch.samples.signalsample.Sample
+
+     
